cd3126f06669bd2eeb0dbb665f8e6565442fc9a5
haxxan-hshs <hb0850350@gmail.com>
2026-05-18 17:54:36 +0500

    now commit

--- supabase-config.js
+++ supabase-config.js
@@ -4,11 +4,11 @@
 
 const SUPABASE_URL =
   window.SUPABASE_URL ||
-  'https://dnevnwmlddgwwvnvkwqb.supabase.co';
+  'https://mknkqrtxdosxqmaticbg.supabase.co';
 
 const SUPABASE_ANON_KEY =
   window.SUPABASE_ANON_KEY ||
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRuZXZud21sZGRnd3d2bnZrd3FiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgyNTU3NjAsImV4cCI6MjA5MzgzMTc2MH0.D0J2D79aVrBGw0pQgILVMkuT1iFVdIpXhWL7xOM2Tao';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1rbmtxcnR4ZG9zeHFtYXRpY2JnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwOTc5MDUsImV4cCI6MjA5NDY3MzkwNX0.iAKGfFFcuNyDfxyq2oi4GwDzFMMgJCwKUSEnNcaOSsU';
 
 if (!window.supabase) {
   throw new Error('Supabase CDN script must be loaded before supabase-config.js');
